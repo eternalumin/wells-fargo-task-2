f0af2ffdbb4c96fe5e0b6a3887d4ef13493782f4
eternalumin <157273800+eternalumin@users.noreply.github.com>
2025-08-29 22:57:48 +0530

    Update Entrypoint.java

--- src/main/java/com/wellsfargo/counselor/Entrypoint.java
+++ src/main/java/com/wellsfargo/counselor/Entrypoint.java
@@ -1,14 +1,38 @@
 package com.wellsfargo.counselor;
 
+import com.wellsfargo.counselor.entities.Advisor;
+import com.wellsfargo.counselor.repositories.AdvisorRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@SpringBootApplication
 public class Entrypoint {
 
     public static void main(String[] args) {
         SpringApplication.run(Entrypoint.class, args);
     }
 
+    // Simple test runner to verify JPA + H2 setup works
+    @Bean
+    public CommandLineRunner demo(AdvisorRepository advisorRepo) {
+        return args -> {
+            // Create and save an advisor
+            Advisor adv = new Advisor(
+                    "Jane",
+                    "Doe",
+                    "123 Main St",
+                    "555-1234",
+                    "jane@wf.com",
+                    null
+            );
+            advisorRepo.save(adv);
+
+            // Print all advisors from DB
+            advisorRepo.findAll().forEach(a ->
+                    System.out.println("Advisor in DB: " + a.getFirstName() + " " + a.getLastName())
+            );
+        };
+    }
 }
