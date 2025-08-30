3176dcf8aa9af0a5ec5c2af6d7fdbd032427030f
eternalumin <157273800+eternalumin@users.noreply.github.com>
2025-08-30 12:36:07 +0530

    Update Entrypoint.java

--- src/main/java/com/wellsfargo/counselor/Entrypoint.java
+++ src/main/java/com/wellsfargo/counselor/Entrypoint.java
@@ -13,26 +13,4 @@ public class Entrypoint {
     public static void main(String[] args) {
         SpringApplication.run(Entrypoint.class, args);
     }
-
-    // Simple test runner to verify JPA + H2 setup works
-    @Bean
-    public CommandLineRunner demo(AdvisorRepository advisorRepo) {
-        return args -> {
-            // Create and save an advisor
-            Advisor adv = new Advisor(
-                    "Jane",
-                    "Doe",
-                    "123 Main St",
-                    "555-1234",
-                    "jane@wf.com",
-                    null
-            );
-            advisorRepo.save(adv);
-
-            // Print all advisors from DB
-            advisorRepo.findAll().forEach(a ->
-                    System.out.println("Advisor in DB: " + a.getFirstName() + " " + a.getLastName())
-            );
-        };
-    }
 }
